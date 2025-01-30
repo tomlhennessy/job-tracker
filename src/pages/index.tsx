65d34557d1faf79b725baeae147cb2edd535ae45
Tom Hennessy <tomlhennessy@outlook.com>
2025-01-30 10:06:20 +1000

    github google oauth credentials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
-    Tailwind is Working! 🚀
-  </div>
-  );
-}
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white text-3xl font-bold">
+        Tailwind is Working! 🚀
+      </div>
+    );
+  }
